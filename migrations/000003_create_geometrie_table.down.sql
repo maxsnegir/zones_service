2ccaf2ce4d7c6860b29527ec4141d8c9dceb2f2b
Max Snegirev <goshasnegir@icloud.com>
2023-12-04 12:50:24 +0300

    Change migrations

--- migrations/000003_create_geometrie_table.down.sql
+++ migrations/000003_create_geometrie_table.down.sql
@@ -1,2 +1,3 @@
 DROP INDEX IF EXISTS zone_geometry_idx;
+DROP INDEX IF EXISTS zone_geometry_geom_idx;
 DROP TABLE IF EXISTS zone_geometry;
--- migrations/000003_create_geometrie_table.up.sql
+++ migrations/000003_create_geometrie_table.up.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS zone_geometry
 (
     id   SERIAL PRIMARY KEY,
-    zone_id int REFERENCES zone (id),
+    zone_id int REFERENCES zone (id) ON DELETE CASCADE,
     geom GEOMETRY,
     properties json
 );
 CREATE INDEX IF NOT EXISTS zone_geometry_geom_idx ON zone_geometry USING GIST (geom);
+CREATE INDEX IF NOT EXISTS zone_geometry_idx ON zone_geometry (zone_id);
